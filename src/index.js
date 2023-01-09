e53ca7274b5eff966e8949402dccfb76682bda60
Tung <TungLV.B20PT185@stu.ptit.edu.vn>
2023-01-09 23:15:30 +0700

    static file $ scss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const {engine}  = require('express-handlebars');
 const app = express()
 const port = 3000
+app.use(express.static(path.join(__dirname,'public')))
 app.use(morgan('combined'))
 app.engine('hbs', engine({
   extname: '.hbs'
